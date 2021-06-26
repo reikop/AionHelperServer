e4281a0a4e1ac7bfe3f44400327bb16e44de19e0
Ray Park <reikop.com@gmail.com>
2021-06-26 12:35:08 +0900

    cors 추가

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var logger = require('morgan');
-const args = require('args-parser')(process.argv);
 
 var indexRouter = require('./routes/index');
 var app = express();
@@ -8,6 +7,13 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://reikop.github.io/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
 
 app.use('/', indexRouter);
 module.exports = app;
