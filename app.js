8a77c70b220ffa8defeaa7852c326c1d7fd2c0b6
Ray Park <reikop.com@gmail.com>
2021-06-26 12:36:18 +0900

    cors 추가

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://reikop.github.io/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://reikop.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
