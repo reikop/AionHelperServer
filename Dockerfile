68f26675f9cb7e68244ca34781b0483a2f44ab03
박성현 <reikop.com@gmail.com>
2022-01-18 01:33:22 +0900

    ARGS > ENV

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ COPY package.json /usr/src/bot
 RUN npm install
 COPY . /usr/src/bot
 
-ENTRYPOINT node bin/www --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password="$DB_PASSWORD" --database=$DB_NAME --sslkey="$HTTP_TLS_KEY" --sslcert="$HTTP_TLS_CERT"
+EXPOSE 8081
+
+ENTRYPOINT node bin/www
--- database/index.js
+++ database/index.js
@@ -1,14 +1,22 @@
 // const sqlite3 = require('sqlite3').verbose();
 const mariadb = require('mariadb');
-const args = require('args-parser')(process.argv);
+// const args = require('args-parser')(process.argv);
+
+const HOST = process.env.DB_HOST;
+const PORT = process.env.DB_PORT;
+const USER = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASSWORD;
+const NAME = process.env.$DB_NAME;
+
 class Database {
     constructor() {
+
         this.pool = mariadb.createPool({
-            host: args.host,
-            port: args.port,
-            user: args.user,
-            password: args.password,
-            database: args.database
+            host: HOST,
+            port: PORT,
+            user: USER,
+            password: PASSWORD,
+            database: NAME
         });
         // this.pool = new sqlite3.Database('./aionhelper.db');
         this.createTable();
