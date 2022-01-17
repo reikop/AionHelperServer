f089f3275945d74341c83990c33510d1318f551d
박성현 <reikop.com@gmail.com>
2022-01-18 02:10:03 +0900

    ARGS > ENV

--- database/index.js
+++ database/index.js
@@ -6,7 +6,7 @@ const HOST = process.env.DB_HOST;
 const PORT = process.env.DB_PORT;
 const USER = process.env.DB_USER;
 const PASSWORD = process.env.DB_PASSWORD;
-const NAME = process.env.$DB_NAME;
+const NAME = process.env.DB_NAME;
 
 class Database {
     constructor() {
