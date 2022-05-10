47b3482def9e5ae7a6cbd6a6f6c108cce7afa843
박성현 <reikop.com@gmail.com>
2022-05-10 10:05:36 +0900

    종료 날짜이후 광고는 쿼리되지 않도록 수정

--- database/ads.js
+++ database/ads.js
@@ -2,7 +2,7 @@
 const database = require('./index')
 module.exports = {
     async getAdslist() {
-        return await database.query("SELECT * FROM  ads order by rand() ");
+        return await database.query("SELECT * FROM  ads where enddt > now() order by rand() ");
     },
 
     async getURL(request, id) {
@@ -11,4 +11,4 @@ module.exports = {
         await database.insert("INSERT INTO ads_log (ip_addr, ads_idx, agent) VALUES (?, ? , ?)", [ip ,id,userAgent]);
         return await database.query("SELECT url FROM  ads WHERE idx = ?", [id]);
     },
-}
+}정
